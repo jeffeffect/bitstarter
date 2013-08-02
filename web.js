0b7410eebb4c83b472c09d226fefaa19b5c3f7fd
Jeff Harrison <JWHarrison.cloud@gmail.com>
2013-08-02 23:20:06 +0000

    html update1

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
+var buffer = new Buffer(fs.writeFileSync('index.html'));
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
