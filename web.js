a0ccb86c1e69739996848aaf1ea54815591bbb97
Jeffrey Harrison <JWHarrison.cloud@gmail.com>
2013-08-05 00:47:46 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
-var buffer = new Buffer(fs.writeFileSync('index.html'));
+var fs = require("fs");
+var mybuf = fs.readFileSync('index.html');
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(mybuf.toString('utf-8'));
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
